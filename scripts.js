99152fe1554726d7b1ea2c7f17f28d0ccf9234d9
alexcr11 <a.cr11006@gmail.com>
2024-02-16 01:34:05 -0500

    changed up font on site, added a scroll to top button, positioned scroll to top button for both mobile and web, got rid of old scroll to top area and replaced with message saying thanks for visiting my site got rid of weird cursor dot on site and is back to original cursor thats on the clients machine

--- scripts.js
+++ scripts.js
@@ -3,41 +3,6 @@ document.querySelector('.hamburger').addEventListener('click', function () {
     document.querySelector('.nav-links').classList.toggle('show');
 });
 
-const cursorDot = document.querySelector("[data-cursor-dot]");
-const cursorOutline = document.querySelector("[data-cursor-outline]");
-
-window.addEventListener("mousemove", function (e) {
-
-    const posX = e.clientX;
-    const posY = e.clientY;
-
-    cursorOutline.style.left = `${posX}px`;
-    cursorOutline.style.top = `${posY}px`;
-
-    cursorDot.animate({
-        left: `${posX}px`,
-        top: `${posY}px`
-    }, { duration: 500, fill: "forwards" });
-});
-
-document.querySelectorAll('a').forEach((link) => {
-    link.addEventListener('mouseover', () => {
-        // Change color when mouse is over an <a> element
-        cursorDot.style.backgroundColor = 'rgb(137, 31, 158)'; // Change to your desired color
-        cursorOutline.style.borderColor = 'rgb(137, 31, 158)'; // Change to your desired color
-        cursorDot.classList.add('expanded');
-        cursorOutline.classList.add('expanded');
-    });
-
-    link.addEventListener('mouseout', () => {
-        // Revert to default color when mouse leaves the <a> element
-        cursorDot.style.backgroundColor = 'white';
-        cursorOutline.style.borderColor = 'hsla(0, 0%, 100%, 0.5)';
-        cursorDot.classList.remove('expanded');
-        cursorOutline.classList.remove('expanded');
-    });
-});
-
 // JavaScript
 const navToggle = document.querySelector('.navToggle');
 const hamburger = document.getElementById('hamburger');
@@ -81,3 +46,13 @@ function SendMail() {
         alert("Success! " + res.status);
     })
 }
+
+window.addEventListener('scroll', function() {
+    var scrollButton = document.getElementById('scrollButton');
+    if (window.pageYOffset > 0) {
+        scrollButton.style.display = "block";
+
+    } else {
+        scrollButton.style.display = "none";
+    }
+});
